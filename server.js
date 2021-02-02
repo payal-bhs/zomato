f940e149e934da9bedaa2266a8848f9ad477f460
Payal Patel <ppatel@bhs-consultants.com>
2021-02-02 07:51:39 -0500

    play around with server

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
