d94c26a7619c7eb8099885d8066db167b44747fe
Payal Patel <ppatel@bhs-consultants.com>
2021-02-02 05:37:01 -0500

    add style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import React, { useState, useEffect } from "react";
 // import { Counter } from "./features/counter/Counter";
 import "./App.scss";
 import { SearchFormContainer } from "./components/SearchFormContainer";
@@ -7,20 +8,49 @@ import store from "./store/store";
 import RestaurantContainer from "./components/RestaurantContainer";
 
 const App = () => {
-  return (
-      <Provider store={store}>
-          <div className="App">
-              <header className="App-header">
-                  {/* <Counter /> */}
-                  <h1>Zomato Restaurants</h1>
-                  <SearchFormContainer />
-              </header>
-              <section>
-                  <RestaurantContainer />
-              </section>
-          </div>
-      </Provider>
-  );
+    // const [dimensions, setDimensions] = useState({
+    //     height: window.innerHeight,
+    //     width: window.innerWidth
+    // });
+
+    // useEffect(() => {
+    //     const debouncedHandleResize = debounce(function handleResize() {
+    //         setDimensions({
+    //             height: window.innerHeight,
+    //             width: window.innerWidth
+    //         });
+    //     }, 1000);
+    //     window.addEventListener("resize", debouncedHandleResize);
+
+    //     return _ => {
+    //         window.removeEventListener("resize", debouncedHandleResize);
+    //     };
+    // });
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <header className="App-header">
+                    {/* <Counter /> */}
+                    <h1>Zomato Restaurants</h1>
+                    <SearchFormContainer />
+                </header>
+                <main className="main">
+                    <RestaurantContainer />
+                </main>
+            </div>
+        </Provider>
+    );
 };
+
+// const debounce = (fn, ms) => {
+//     let timer;
+//     return _ => {
+//         clearTimeout(timer);
+//         timer = setTimeout(_ => {
+//             timer = null;
+//             fn.apply(this, arguments);
+//         }, ms);
+//     };
+// };
 
 export default App;
--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,27 +1,109 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./RestaurantContainer.style.scss";
+// import { Link } from "react-router-dom";
 
 const RestaurantContainer = () => {
     const stateRestaurant = useSelector(state => state.restaurant);
+    const stateSearch = useSelector(state => state.search);
     console.log("RestaurantContainer => ", stateRestaurant);
     // list of search restaurants
-    return stateRestaurant.loading ? (
-        <h2>Loading</h2>
-    ) : stateRestaurant.error ? (
-        <h2>{stateRestaurant.error}</h2>
-    ) : (
-        <div>
-            <h2>Restaurant List</h2>
-            {(stateRestaurant?.restaurants.length > 0) ?
-            <div>
-                {stateRestaurant?.restaurants?.map((restaurant, key) =>
-                    <p key={key}>{restaurant.name}</p>
-                )}
-            </div>
-            : <div>No Result Found</div>}
+    return (
+        <div className="wrapper">
+            {stateRestaurant.loading ? (
+                    <h2>Loading</h2>
+                ) : stateRestaurant.error ? (
+                    <h2>{stateRestaurant.error}</h2>
+                ) : (
+                    (stateRestaurant?.restaurants.length > 0) ?
+                        <ul className="cards">
+                            {stateRestaurant?.restaurants?.map((restaurant, key) =>
+                                <li class="cards_item restaurant-cards" key={key}>
+                                    <RestaurantCard key={key} restaurant={restaurant.restaurant}  />
+                                </li>
+                            )}
+                        </ul>
+                    : (stateSearch.city !== "") ?
+                        <div>No Result Found</div>
+                        : null
+            )}
         </div>
-
     );
 };
 
 export default RestaurantContainer;
+
+export const RestaurantCard = (restaurant) => {
+    console.log("restaurant => ", restaurant);
+    return (
+        <figure className="restaurant-card">
+            <figcaption className="card-body">
+                <a href={restaurant.restaurant.url} target="_blank" rel="noreferrer">
+                    <h2 className="card_title">{restaurant.restaurant.name}</h2>
+                </a>
+                <p className="card_text">{restaurant.restaurant.location.locality_verbose}</p>
+
+                <p className="small_card_text">{restaurant.restaurant.cuisines}</p>
+                <div className="rating">
+                    <Rating userRating={restaurant.restaurant.user_rating.aggregate_rating} />
+                </div>
+            </figcaption>
+        </figure>
+    );
+};
+
+const Rating = (userRating = 0) => {
+    const rating = Math.round(parseInt(userRating.userRating, 10) * 2) / 2;
+    let output = [];
+    let index = rating;
+
+    // Append all the filled whole stars
+    for (index = rating; index >= 1; index--) {
+        output.push("fa fa-star checked");
+    }
+
+    // If there is a half a star, append it
+    if (index === .5) {
+        output.push("fa fa-star-half-o checked");
+    }
+
+    // Fill the empty stars
+    for (let i = (5 - rating); i >= 1; i--)
+        output.push("fa fa-star-o");
+
+    return (
+        <ul>
+            {output.map((rating, key) =>
+                <li key={key}>
+                    <i className={rating}></i>
+                </li>
+            )}
+        </ul>
+    )
+}
+
+// export const RestaurantCard = (restaurant) => {
+//     console.log("restaurant => ", restaurant);
+//     return (
+//         <figure className="card">
+//             <div className="card_image">
+//                 <img data-src={restaurant.restaurant.thumb} alt={restaurant.restaurant.cuisines}  />
+//             </div>
+//             <figcaption className="card_content">
+//                 <h2 className="card_title">{restaurant.restaurant.name}</h2>
+//                 {/* <p className="card_text">{restaurant.restaurant.location.locality_verbose}</p> */}
+//                 <p className="card_text">{restaurant.restaurant.cuisines}</p>
+//                 <div className="rating">
+//                     <ul>
+//                         <li><i className="fa fa-star checked"></i></li>
+//                         <li><i className="fa fa-star checked"></i></li>
+//                         <li><i className="fa fa-star checked"></i></li>
+//                         <li><i className="fa fa-star"></i></li>
+//                         <li><i className="fa fa-star"></i></li>
+//                     </ul>
+//                 </div>
+//                 <button className="btn card_btn">Read More</button>
+//             </figcaption>
+//         </figure>
+//     );
+// };
